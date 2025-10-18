4bc5f80db52cda8fd3e8ff6e02c4c3278766a901
2025d2r045-ui <2025d2r045@mietjammu.in>
2025-10-18 12:54:34 +0530

    Add files via upload

---
+++ 3.(a) No. is even or odd.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   int a;
+    printf("Enter Number: ");
+    scanf("%d", &a);
+    if(a%2==0)
+        printf("Number is Even");
+     else{
+        printf("Number is Odd");
+     }
+    return 0;
+}
+                                                                                                                                     
